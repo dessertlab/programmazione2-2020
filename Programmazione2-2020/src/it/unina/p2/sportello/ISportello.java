f81081ee34ba77e855001df42dc4227bd4e82426
Roberto Natella <roberto.natella@unina.it>
2020-05-04 17:51:38 +0200

    Fix package esercizio "Broker/Sportello"

---
+++ Programmazione2-2020/src/it/unina/p2/sportello/ISportello.java
@@ -0,0 +1,6 @@
+package it.unina.p2.sportello;
+
+public interface ISportello {
+
+	void serviRichiesta(int clientID, int requestID);
+}
---
+++ Programmazione2-2020/src/it/unina/p2/sportello/SportelloProxy.java
@@ -0,0 +1,58 @@
+package it.unina.p2.sportello;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class SportelloProxy implements ISportello {
+
+	
+	private DatagramSocket UDPsocket;
+	private int port = 6000;
+
+	
+	public SportelloProxy() {
+		try {
+			UDPsocket = new DatagramSocket();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+	public int getPort() {
+		return this.port;
+	}
+	
+	
+	@Override
+	public void serviRichiesta(int clientID, int requestID) {
+
+		String request_str = "SERVI " + clientID + " " + requestID;
+		
+		byte [] request_data = request_str.getBytes();
+		
+		int request_len = request_data.length;
+		
+		
+		try {
+			DatagramPacket UDPpacket = new DatagramPacket(request_data, request_len, InetAddress.getLocalHost(), this.port);
+
+			UDPsocket.send(UDPpacket);
+				
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
